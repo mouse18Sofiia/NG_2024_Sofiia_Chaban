3b09adf25e428eefe0d673ab61e9759c34dbb06a
Sofiia Chaban <sonyamouse05@gmail.com>
2024-02-01 11:10:37 +0200

    Added second task of the first lection

---
+++ lection_1/Task_2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int salary = -1;
+    cin >> salary;
+
+    if (salary < 1000) {
+        cout << "Work more!" << endl;
+    }
+
+    if (!(salary < 1000) && !(salary < 1000000) && !(salary > 1000000)) {
+        cout << "Good job!" << endl;
+    }
+
+    if (!(salary < 1000) && !(salary < 1000000) && (salary > 1000000)) {
+        cout << "You are a millionaire!" << endl;
+    }
+
+    cout << "...but you're great!" << endl;
+    return 0;
+}
